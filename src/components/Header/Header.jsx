1178145eb1e32527f10c943f5875d1e97db818dd
GuillaumeN <guillaume.nivesse@yahoo.fr>
2022-02-17 16:37:52 +0100

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link, NavLink } from 'react-router-dom'
+import logo from '../../assets/logoprev33.png'
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="Header">
+      <div className="logo-container">
+          <Link to='/'><img className='Header-logo' src={logo} alt="logo" /></Link>
+      </div>
+          <p>"Protégeons ensemble votre outil de travail" <br/>
+          Incendie - Risques Professionnels - Sûreté </p>
+    </div>    
+)
+}
+
+export default Header
