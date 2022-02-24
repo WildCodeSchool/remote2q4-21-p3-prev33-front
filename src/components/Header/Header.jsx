762127b2423a6e99f554cc74393cbc639b2fc0d4
GuillaumeN <guillaume.nivesse@yahoo.fr>
2022-02-24 14:24:33 +0100

    first commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom'
-import logo from '../../assets/logoprev33.png'
+import { Link } from 'react-router-dom'
+import logo3 from '../../assets/logoprev33.png'
 import './Header.css';
 
 function Header() {
   return (
     <div className="Header">
-      <div className="logo-container">
-          <Link to='/'><img className='Header-logo' src={logo} alt="logo" /></Link>
+      <div className='Logo'>
+        <Link to='/'><img className='Header-logo' src={logo3} alt="logo3" /></Link>
       </div>
-          <p>"Protégeons ensemble votre outil de travail" <br/>
-          Incendie - Risques Professionnels - Sûreté </p>
-    </div>    
-)
+
+      <div className='text1'>
+        <p>"Protégeons, ensemble,</p>
+        <p>votre outil de travail"</p>
+      </div>
+      <div className='text2'>
+        <p>Incendie</p>
+        <br />
+        <p>Risques Professionnels</p>
+        <br />
+        <p>Sûreté</p>
+      </div>
+      <div className='Bouton'>
+        <button class="favorite styled" type="button">
+          Contactez-nous
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Header
