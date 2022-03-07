58d3b4abee082798701cec30c6e518f4acef80b3
belot nicolas <nicolas-belot_student2021@wilder.school>
2022-03-07 09:25:24 +0100

    mergin dev

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,14 +3,16 @@ import Caroussel from '../../components/caroussel/Caroussel';
 import { SliderData } from '../../components/caroussel/SliderData';
 import imagePompier from '../../assets/pompier.jpg'
 import './Home.css'
+import CardsFormation from '../../components/cards/CardsFormation';
 
 const Home = () => {
     return (
         <div className='home'>
-            <h1>Accueil</h1>
             <div className="img">
                 <Caroussel slides={SliderData} />
-                <img className='imagePompier' src={imagePompier} alt="logo" />
+            </div>
+            <div className="formation-card">
+                <CardsFormation />
             </div>
 
         </div>
